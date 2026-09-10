1b94409975af1ab1e45ad9087e5be34ed4eba4f4
Prerit Srivastava <srivastavaprerit11@gmail.com>
2026-09-10 21:15:20 +0530

    Delete Sql_queries/risk_by_sub_grade_analysis.sql

--- Sql_queries/risk_by_sub_grade_analysis.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    sub_grade,
-    COUNT(*) AS n_loans,
-    ROUND(AVG(interest_rate)::numeric, 2) AS avg_interest_rate,
-    ROUND(100.0 * SUM(CASE WHEN loan_status = 'Charged Off' THEN 1 ELSE 0 END) / COUNT(*), 2) AS default_rate_pct
-FROM lc_loans
-GROUP BY sub_grade
-ORDER BY sub_grade;
