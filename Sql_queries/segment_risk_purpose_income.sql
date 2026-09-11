553ef5be29985c0d165def0699388cf905ddd110
Prerit Srivastava <srivastavaprerit11@gmail.com>
2026-09-11 15:17:24 +0530

    Segment risk analysis by Purpose & Income

---
+++ Sql_queries/segment_risk_purpose_income.sql
@@ -0,0 +1,15 @@
+SELECT 
+    t1.purpose,
+    CASE 
+        WHEN t2.annual_income < 40000 THEN 'Low'
+        WHEN t2.annual_income < 90000 THEN 'Mid'
+        ELSE 'High'
+    END AS income_bracket,
+    COUNT(*) AS n_loans,
+    ROUND((100.0 * SUM(CASE WHEN t1.loan_status = 'Charged Off' THEN 1 ELSE 0 END) / COUNT(*))::numeric, 2) AS default_rate_pct
+FROM lc_loans t1
+JOIN lc_borrowers t2 ON t1.borrower_id = t2.borrower_id
+GROUP BY t1.purpose, income_bracket
+HAVING COUNT(*) > 500
+ORDER BY default_rate_pct DESC
+LIMIT 20;
