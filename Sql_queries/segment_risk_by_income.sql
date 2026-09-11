2a96a4a4e69ae92a6e0c15ed0f5c739803dc483d
Prerit Srivastava <srivastavaprerit11@gmail.com>
2026-09-11 15:18:54 +0530

    Segment risk analysis by Income

---
+++ Sql_queries/segment_risk_by_income.sql
@@ -0,0 +1,13 @@
+-- Default Rate by Income Bracket
+SELECT 
+    CASE 
+        WHEN t2.annual_income < 40000 THEN 'Low'
+        WHEN t2.annual_income < 90000 THEN 'Mid'
+        ELSE 'High'
+    END AS income_bracket,
+    COUNT(*) AS n_loans,
+    ROUND((100.0 * SUM(CASE WHEN t1.loan_status = 'Charged Off' THEN 1 ELSE 0 END) / COUNT(*))::numeric, 2) AS default_rate_pct
+FROM lc_loans t1
+JOIN lc_borrowers t2 ON t1.borrower_id = t2.borrower_id
+GROUP BY income_bracket
+ORDER BY default_rate_pct DESC;
