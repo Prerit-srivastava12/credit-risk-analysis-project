67c09603d696f04edf88e7b8e8e50357550356aa
Prerit Srivastava <srivastavaprerit11@gmail.com>
2026-09-15 17:11:41 +0530

    Updated query

--- Sql_queries/Risk_by_bureau_score_band_analysis.sql
+++ Sql_queries/Risk_by_bureau_score_band_analysis.sql
@@ -8,5 +8,6 @@ SELECT
     ROUND((100.0 * AVG(t1.loan_default))::numeric, 2) AS default_rate_pct
 FROM lt_loans t1
 JOIN lt_borrowers t2 ON t1.borrower_id = t2.borrower_id
+WHERE t2.bureau_score > 0
 GROUP BY score_band
 ORDER BY default_rate_pct DESC;
